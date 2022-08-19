863dcb2eb3b5bb2c26311b4e4ad8bcdc744f7ad8
Muhammad Fadhil Akbar <82107511+MuhammadFadhil10@users.noreply.github.com>
2022-08-19 18:22:34 +0700

    Update blogs (#4)
    * add update blog method
    
    * add update blog logic
    
    * create edit blog router
    
    * finishing update blog logic

--- controller/blog.js
+++ controller/blog.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const Blog = require('../models/blog');
 
 const createBlog = async (req, res) => {
@@ -10,4 +11,33 @@ const createBlog = async (req, res) => {
 		console.log(error);
 	}
 };
+
+const updateBlog = async (req, res) => {
+	const blogId = new ObjectId(req.params.blogId);
+	const updatedTitle = req.body.title;
+	const updatedThumbnail = req.body.thumbnail;
+	const updatedContent = req.body.content;
+	const isAnonymous = req.body.isAnonymous;
+	const updatedTag = req.body.tag;
+
+	try {
+		await Blog.update(
+			blogId,
+			updatedTitle,
+			updatedThumbnail,
+			updatedContent,
+			isAnonymous,
+			updatedTag
+		);
+		return res
+			.json({
+				message: 'blog edited!',
+			})
+			.status(200);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 exports.createBlog = createBlog;
+exports.updateBlog = updateBlog;
--- router/blog.js
+++ router/blog.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { createBlog } = require('../controller/blog');
+const { createBlog, updateBlog } = require('../controller/blog');
 
 // Post
 router.post('/create-blog', createBlog);
+
+// update
+router.patch('/edit-blog/:blogId', updateBlog);
 
 module.exports = router;

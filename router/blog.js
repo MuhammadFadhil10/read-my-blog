c72935a370878ac90e9b57fa8d68b59ffb662253
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-19 19:26:22 +0700

    create a router for find blog by id

--- router/blog.js
+++ router/blog.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { createBlog, updateBlog } = require('../controller/blog');
+const { createBlog, updateBlog, findById } = require('../controller/blog');
 
 // Post
 router.post('/create-blog', createBlog);
+
+// get
+router.get('/blog/:blogId', findById);
 
 // update
 router.patch('/edit-blog/:blogId', updateBlog);

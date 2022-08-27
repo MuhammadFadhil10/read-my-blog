7f96a35b5d8c4a1cf740d944f2566b21e2cca3b0
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-27 13:27:52 +0700

    passing register logic to register router

--- router/auth.js
+++ router/auth.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const Validate = require('../utils/authHelper');
+const { register } = require('../controller/auth');
 
-router.post('/register', Validate.email('email'));
+router.post('/register', Validate.email('email'), register);
 
 module.exports = router;

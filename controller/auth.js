5e23b181f5a31aeb1c2c89ab649e07dc5863dca5
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-27 13:03:00 +0700

    create error handling for register request

--- controller/auth.js
+++ controller/auth.js
@@ -1,1 +1,12 @@
-const {} = require('express-validator');
+const { validationResult } = require('express-validator');
+
+const register = (req, res) => {
+	const { email, userName, password, confirmPassword } = req.body;
+	const error = validationResult();
+	if (!error.isEmpty) {
+		return res.json({
+			status: 'error',
+			message: error.array().map((err) => err.msg),
+		});
+	}
+};

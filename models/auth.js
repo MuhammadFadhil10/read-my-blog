3634e3d408759e753612102ce4e6b50e8591d276
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-27 15:31:05 +0700

    refactor find user method

--- models/auth.js
+++ models/auth.js
@@ -9,7 +9,12 @@ class Auth {
 	createUser() {
 		return mongo().collection('users').insertOne(this);
 	}
-	static findById(id) {
-		return mongo().collection('users').findOne({ _id: id });
+	static find(filter, value) {
+		if (filter === 'id') {
+			return mongo().collection('users').findOne({ _id: value });
+		} else if (filter === 'email') {
+			return mongo().collection('users').findOne({ email: value });
+		}
+		return mongo().collection('users').findOne({ userName: value });
 	}
 }

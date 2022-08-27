6fdb41eda567eccac24099b66d95fafda7bd635e
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-27 13:35:30 +0700

    finishing email input validation

--- controller/auth.js
+++ controller/auth.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 const register = (req, res) => {
 	const { email, userName, password, confirmPassword } = req.body;
 	const error = validationResult(req);
-	if (!error.isEmpty) {
+	if (!error.isEmpty()) {
 		return res.json({
 			status: 'error',
 			message: error.array().map((err) => err.msg),

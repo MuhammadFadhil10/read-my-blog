aa62e14c8963498cfee2dcc9bf4fcd5638c26a3f
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-27 15:36:26 +0700

    add check if user already register logic

--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,8 @@
 const { validationResult } = require('express-validator');
 const { hash, compare } = require('bcryptjs');
 
+const Auth = require('../models/auth');
+
 const register = async (req, res) => {
 	const { email, userName, password, confirmPassword } = req.body;
 	const error = validationResult(req);
@@ -11,8 +13,14 @@ const register = async (req, res) => {
 		});
 	}
 
-	const hashedPassword = await hash(password.toString(), 12);
-	console.log(hashedPassword);
+	const emailExist = await Auth.find('email', email);
+	const userNameExist = await Auth.find('email', userName);
+
+	console.log(emailExist);
+	console.log(userNameExist);
+
+	// const hashedPassword = await hash(password.toString(), 12);
+	// console.log(hashedPassword);
 
 	return res.json({
 		status: 'success',

350d0af73d6eb80c54c9fd2453460ca7d307cfe5
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-27 15:13:11 +0700

    passing all validation method to register router

--- router/auth.js
+++ router/auth.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const Validate = require('../utils/authHelper');
 const { register } = require('../controller/auth');
 
-router.post('/register', Validate.email('email'), register);
+router.post(
+	'/register',
+	Validate.email('email'),
+	Validate.userName('userName'),
+	Validate.password('password'),
+	Validate.confirmPassword('confirmPassword'),
+	register
+);
 
 module.exports = router;

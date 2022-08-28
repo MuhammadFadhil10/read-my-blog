45612ca767128b9907b9e44b7f8a75137dee5928
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-28 21:03:51 +0700

    finishing search user logic

--- controller/user.js
+++ controller/user.js
@@ -4,7 +4,30 @@ const searchUser = async (req, res) => {
 	const value = req.query.v;
 	try {
 		const users = await User.searchUser(value).toArray();
-		console.log(users);
+		if (users.length === 0) {
+			return res
+				.json({
+					status: 'error',
+					message: 'User not found',
+				})
+				.status(404);
+		}
+		return res
+			.json({
+				status: 'success',
+				totalResult: users.length,
+				users: users.map((user) => {
+					return {
+						profilePicture: user.profilePicture,
+						userName: user.userName,
+						name: user.name,
+						bio: user.bio,
+						likedTopics: user.likedTopics,
+						web: user.web,
+					};
+				}),
+			})
+			.status(404);
 	} catch (error) {
 		console.log(error);
 	}

c1db01a765c6317e061e6e2591085b1d06b54a8f
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-28 19:38:24 +0700

    add name condition to search user method

--- models/auth.js
+++ models/auth.js
@@ -15,6 +15,8 @@ class Auth {
 			return mongo().collection('users').findOne({ _id: value });
 		} else if (filter === 'email') {
 			return mongo().collection('users').findOne({ email: value });
+		} else if (filter === 'name') {
+			return mongo().collection('users').findOne({ name: value });
 		}
 		return mongo().collection('users').findOne({ userName: value });
 	}

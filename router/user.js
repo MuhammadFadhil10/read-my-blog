2d674d3f6b842093672aa202f26c83fbc3d06571
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-28 20:52:24 +0700

    create search user router

--- router/user.js
+++ router/user.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { searchUser } = require('../controller/user');
+
+router.get('/user/s', searchUser);
+
 module.exports = router;

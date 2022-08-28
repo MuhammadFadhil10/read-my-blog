ce0db4f6cd6facd2505cd10ca57d87f53d190033
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-28 20:49:24 +0700

    complement search user logic

--- controller/user.js
+++ controller/user.js
@@ -1,1 +1,13 @@
 const User = require('../models/user');
+
+const searchUser = async (req, res) => {
+	const value = req.query.v;
+	try {
+		const users = await User.searchUser(value).toArray();
+		console.log(users);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+exports.searchUser = searchUser;

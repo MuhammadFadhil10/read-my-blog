87450f1a3c03ad40a48ebc918d7d1923611811f9
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-29 15:42:20 +0700

    create edit profile logic

--- controller/user.js
+++ controller/user.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const User = require('../models/user');
 
 const searchUser = async (req, res) => {
@@ -33,4 +34,25 @@ const searchUser = async (req, res) => {
 	}
 };
 
+const editProfile = async (req, res) => {
+	const userId = new ObjectId(req.params.userId.trim());
+	const { profilePicture, userName, name, bio, web, likedTopics } = req.body;
+	let oldProfileData = null;
+	try {
+		const userNameExist = await User.findUser('username', userName);
+		if (userNameExist) {
+			return res.json({
+				status: 'error',
+				message: 'Username already exist!',
+			});
+		}
+		oldProfileData = await User.findUser('id', userId);
+		console.log(typeof oldProfileData.likedTopics);
+		console.log(typeof likedTopics);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 exports.searchUser = searchUser;
+exports.editProfile = editProfile;

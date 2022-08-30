0a55c4736c36a0abbe4a62fd99df6afecd3ae2d8
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-30 17:59:36 +0700

    change  edit profile fumction

--- utils/editProfileHelper.js
+++ utils/editProfileHelper.js
@@ -9,41 +9,15 @@ const updateProfileHelper = (
 	web,
 	likedTopics
 ) => {
-	// check if old "liked topics" is array
-	if (Array.isArray(oldProfileData.likedTopics)) {
-		if (Array.isArray(likedTopics)) {
-			return UserClass.updateProfile(
-				userId,
-				profilePicture ? profilePicture : oldProfileData.profilePicture,
-				userName ? userName : oldProfileData.userName,
-				name ? name : oldProfileData.name,
-				bio ? bio : oldProfileData.bio,
-				web ? web : oldProfileData.web,
-				[...oldProfileData.likedTopics, ...likedTopics]
-			);
-		} else {
-			return UserClass.updateProfile(
-				userId,
-				profilePicture ? profilePicture : oldProfileData.profilePicture,
-				userName ? userName : oldProfileData.userName,
-				name ? name : oldProfileData.name,
-				bio ? bio : oldProfileData.bio,
-				web ? web : oldProfileData.web,
-				[...oldProfileData.likedTopics, likedTopics]
-			);
-		}
-	} else {
-		// where old liked topics not array
-		return UserClass.updateProfile(
-			userId,
-			profilePicture ? profilePicture : oldProfileData.profilePicture,
-			userName ? userName : oldProfileData.userName,
-			name ? name : oldProfileData.name,
-			bio ? bio : oldProfileData.bio,
-			web ? web : oldProfileData.web,
-			Array.isArray(likedTopics) ? likedTopics : [likedTopics]
-		);
-	}
+	return UserClass.updateProfileHelper(
+		userId,
+		profilePicture ? profilePicture : oldProfileData.profilePicture,
+		userName ? userName : oldProfileData.userName,
+		name ? name : oldProfileData.name,
+		bio ? bio : oldProfileData.bio,
+		web ? web : oldProfileData.web,
+		likedTopics
+	);
 };
 
 module.exports = updateProfileHelper;

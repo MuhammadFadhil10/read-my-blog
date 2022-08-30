e598c19516992607b834a95c4c1f7f756edc54fb
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-30 17:51:44 +0700

    finishing  edit profile fumction

--- utils/editProfileHelper.js
+++ utils/editProfileHelper.js
@@ -1,13 +1,23 @@
-const updateProfile = (UserClass,oldProfileData,userId,profilePicture,userName,name,bio,web,likedTopics) => {
-    await UserClass.updateProfile(
-        userId,
-        profilePicture ? profilePicture : oldProfileData.profilePicture,
-        userName ? userName : oldProfileData.userName,
-        name ? name : oldProfileData.name,
-        bio ? bio : oldProfileData.bio,
-        web ? web : oldProfileData.web,
-        [...oldProfileData.likedTopics, ...likedTopics]
-    );
-}
+const updateProfileHelper = (
+	UserClass,
+	oldProfileData,
+	userId,
+	profilePicture,
+	userName,
+	name,
+	bio,
+	web,
+	likedTopics
+) => {
+	return UserClass.updateProfileHelper(
+		userId,
+		profilePicture ? profilePicture : oldProfileData.profilePicture,
+		userName ? userName : oldProfileData.userName,
+		name ? name : oldProfileData.name,
+		bio ? bio : oldProfileData.bio,
+		web ? web : oldProfileData.web,
+		likedTopics
+	);
+};
 
 module.exports = updateProfile;

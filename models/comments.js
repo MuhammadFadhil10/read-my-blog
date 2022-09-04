4df55569b7292e44ab61cbdc0b9548fcd63150ea
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-04 16:25:33 +0700

    finishing basic crud method for comment feature

--- models/comments.js
+++ models/comments.js
@@ -11,6 +11,17 @@ class Comment {
 	upload() {
 		return mongo().collection('comments').insertOne(this);
 	}
+	static findByBlog(blogId) {
+		return mongo().collection('comments').find({ blogId: blogId });
+	}
+	static update(commentId, newText) {
+		return mongo()
+			.collection('comments')
+			.updateOne({ _id: commentId }, { $set: { text: newText } });
+	}
+	static delete(commentId) {
+		return mongo().collection('comments').deleteOne({ _id: commentId });
+	}
 }
 
 module.exports = Comment;

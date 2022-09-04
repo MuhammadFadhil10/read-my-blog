44d3c4efba72bc2d8f0e7b09f60a40c9147bf5f2
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-04 19:04:50 +0700

    create update comment router

--- router/comments.js
+++ router/comments.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { uploadComment, getComment } = require('../controller/comments');
+const {
+	uploadComment,
+	getComment,
+	updateComment,
+} = require('../controller/comments');
 
 router.post('/send-comment/:senderId/:blogId', uploadComment);
 router.get('/comments/:blogId', getComment);
+router.patch('/comment/update/:blogId', updateComment);
 
 module.exports = router;

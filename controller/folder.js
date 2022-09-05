c25859c3042bde25dd9cb829f6012661ee7bc9e0
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-05 21:17:45 +0700

    add feature that add duplicate number to folder name when folder name already exist

--- controller/folder.js
+++ controller/folder.js
@@ -6,15 +6,26 @@ const createFolder = async (req, res) => {
 	let { folderName } = req.body;
 
 	try {
-		// check if foldername already exist
+		// check if folder already exist
 		const hasFolder = await Folder.findByUser(userId).toArray();
 		if (hasFolder.length > 0) {
+			// check if folder name already exist but just one
 			const folderNameExist = hasFolder.filter(
 				(folder) => folder.folderName === folderName
 			);
-			folderNameExist.length > 0
-				? folderName + `(${folderNameExist.length + 1})`
-				: folderName;
+			// check if folder name already duplicate
+			const folderNameDuplicate = hasFolder.filter((folder) =>
+				folder.folderName.includes(`${folderName} (`)
+			);
+			// if folder name already exist but just one, then add (1) to folder name as first duplicate
+			if (folderNameExist.length === 1 && folderNameDuplicate.length === 0) {
+				folderName += ` (1)`;
+			}
+			// if the folder name already more than 1 (have 1 duplicate), then add more duplicate
+			if (folderNameDuplicate.length > 0) {
+				// add more duplicate number to folder name
+				folderName += ` (${folderNameDuplicate.length + 1})`;
+			}
 		}
 		const newFolder = new Folder(folderName, userId);
 		await newFolder.create();

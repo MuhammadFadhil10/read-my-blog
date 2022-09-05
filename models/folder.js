47b115738085953b4038296d07cfef3a1d1beff3
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-05 14:28:58 +0700

    create constructor and create method for folder class

---
+++ models/folder.js
@@ -0,0 +1,16 @@
+const { mongo } = require('../config/mongoConfig');
+
+class Folder {
+	constructor(folderName, userId) {
+		this.folderName = folderName;
+		this.userId = userId;
+		this.createdTime = new Date().toISOString();
+		this.updatedTime = null;
+	}
+
+	create() {
+		return mongo().collection('folders').insertOne(this);
+	}
+}
+
+module.exports = Folder;

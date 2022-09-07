04c9bfd41889eb211db1f25d3a5f3befcd5408a3
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-07 09:52:28 +0700

    add my-folders router

--- router/folder.js
+++ router/folder.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { createFolder } = require('../controller/folder');
+const { createFolder, myFolders } = require('../controller/folder');
 
 router.post('/folder/create/:userId', createFolder);
+router.get('/folder/my-folders/:userId', myFolders);
 
 module.exports = router;

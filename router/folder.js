f143b7b03282b80908d0ca5a4ff7258ac6bd952f
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-07 10:18:42 +0700

    add delete folder router

--- router/folder.js
+++ router/folder.js
@@ -5,10 +5,12 @@ const {
 	createFolder,
 	myFolders,
 	updateFolder,
+	deleteFolder,
 } = require('../controller/folder');
 
 router.post('/folder/create/:userId', createFolder);
 router.get('/folder/my-folders/:userId', myFolders);
 router.patch('/folder/update/:folderId/:userId', updateFolder);
+router.delete('/folder/delete/:folderId', deleteFolder);
 
 module.exports = router;
